d14b8907abf405a8ae9be06a810576673e2e2e31
Jose Alisson Tavares de Oliveira <alisson.dev737@gmail.com>
2024-12-31 20:55:34 -0300

    If

--- Python.py
+++ Python.py
@@ -1,8 +1,8 @@
-# Primeiro desafio
+# First challenge
 '''nome=input('Por favor digite seu nome:\n')
 print('Ola, e um prazer te conhecer, ',nome,'!')'''
 
-#segundo desafio (tipos primitivos)
+#Secund challenge (Primitive types)
 '''n1=int(input('Digite um numero:\n'))
 n2=int(input('Digite mais um numero\n'))
 s=(n1+n2)
@@ -14,6 +14,17 @@ print(name.isalpha())
 print(len(name)==0)'''
 
 #Tests with strings
-algo=input('Insert something: ')
+'''algo=input('Insert something: ')
 print('This date is of the type: ', type(algo))
 print('Are all spaces?: ',algo.isspace())
+print('all are alphabetics? ',algo.isalnum())
+print('all are lowercase? ',algo.islower())'''
+
+
+algo=input('Insert something: ')
+if not algo.isalnum():
+    print('Attention! Your password needs to have just letters and numbers uppercase and lowercase.')
+elif algo.islower():
+    print('Attention! Your password needs to have at least one uppercase letter')
+else:
+    print('Done! Please wait..')
