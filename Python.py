c57bff93da316a6a15c0b94f3cafaba4412573d0
Jose Alisson Tavares de Oliveira <alisson.dev737@gmail.com>
2024-12-30 21:09:15 -0300

    2o commit

--- Python.py
+++ Python.py
@@ -1,3 +1,10 @@
 # Primeiro desafio
-nome=input('Por favor digite seu nome:\n')
-print('Ola, e um prazer te conhecer, ',nome,'!')
+'''nome=input('Por favor digite seu nome:\n')
+print('Ola, e um prazer te conhecer, ',nome,'!')'''
+
+#segundo desafio (tipos primitivos)
+n1=int(input('Digite um numero:\n'))
+n2=int(input('Digite mais um numero\n'))
+s=(n1+n2)
+print()
+print('A soma entre {} e {} sera {}'.format(n1,n2,s))
